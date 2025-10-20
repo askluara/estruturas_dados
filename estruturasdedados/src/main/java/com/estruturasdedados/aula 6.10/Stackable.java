ba86eaec0902872cf1d35e30e2e6054766173686
askluara <luaragabrielleguimaraesaraujo@gmail.com>
2025-10-20 20:31:58 -0300

    Removendo pasta duplicada aula 6.10 (local)

--- estruturasdedados/src/main/java/com/estruturasdedados/aula 6.10/Stackable.java
+++
@@ -1,9 +0,0 @@
-public interface Stackable<T> {
-    T peek();
-    void push(T data);
-    T pop();
-    void update(T newData);
-    boolean isFull();
-    boolean isEmpty();
-    String print();
-}
